414e8e7e98b49eed7f8d9ab381a22e33bbc38585
vprotsenko01 <vprotsenko01@casgate.io>
2025-03-23 17:44:33 +0200

    Init Day18 Sql Injection

---
+++ day18-sql-injection/src/main/kotlin/com/cashir/day18sqlinjection/Day18SqlInjectionApplication.kt
@@ -0,0 +1,11 @@
+package com.cashir.day18sqlinjection
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Day18SqlInjectionApplication
+
+fun main(args: Array<String>) {
+	runApplication<Day18SqlInjectionApplication>(*args)
+}
---
+++ day18-sql-injection/src/test/kotlin/com/cashir/day18sqlinjection/Day18SqlInjectionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cashir.day18sqlinjection
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Day18SqlInjectionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
